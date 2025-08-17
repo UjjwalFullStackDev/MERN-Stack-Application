ada40d4710791533817ecc3b1b11018b433ff9dc
UjjwalFullStackDev <ujjwal.tyagi90611@gmail.com>
2025-08-17 14:45:08 +0530

    Change accessToken time

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -7,7 +7,7 @@ const emailService = require('../services/emailService');
 
 const generateTokens = (userId) => {
   const accessToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '2m'
+    expiresIn: '15m'
   });
   
   const refreshToken = jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
